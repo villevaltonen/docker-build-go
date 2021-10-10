1625efce85e9fb90972db161bd58a99a385153fb
Ville Valtonen <valtonen.vt@gmail.com>
2021-10-10 14:47:13 +0300

    Without Dockerfile

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/api/hello", hello())
-	log.Println("Starting an HTTP-server...")
+	log.Println("HTTP-server is starting...")
 	err := http.ListenAndServe(":8080", r)
 	if err != nil {
 		log.Fatal(err)
